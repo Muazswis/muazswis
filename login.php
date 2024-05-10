2a505af4efe8691fb6be28bd3e6781c61b58a2e3
MUAZ SWİS <muazswis@gmail.com>
2024-05-10 22:13:02 +0300

    odev ilk yukleme
    odevin yukleme deneyimi

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+$kullanici_adi="b231210577@gmail.com";
+$sifre="b231210577";
+
+if($kullanici_adi==$_POST['id'] && $sifre==$_POST['sifre'])
+{
+    echo "Hoşgeldiniz b231210577 <br/>";
+    echo " 7 saniye içinde hakkimda sayfasına yönelendirileceksiniz";
+    header("Refresh: 7; url=hakkimda.html");
+
+}
+else{
+    echo "Girilen bilgiler hatalıdır. 3 saniye içinde giriş ekranına yönelendirileceksiniz";
+    header("Refresh: 3; url=login.html");
+}
+
+?>
