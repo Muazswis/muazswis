e8a58560a30a99870ecd8ecf90064acfc280adfd
MUAZ SWİS <muazswis@gmail.com>
2024-05-14 13:23:32 +0300

    guncelleme
     Bu bağlantı, site kullanıcılarının giriş yapmalarını sağlar.

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?php
 
-$kullanici_adi = "b231210577@gmail.com";
-$sifre = "b231210577";
+$kullanici_adi="b231210577@gmail.com";
+$sifre="b231210577";
 
-if ($kullanici_adi == $_POST['id'] && $sifre == $_POST['sifre']) {
-    header("Refresh: 7; url=hakkimda.html");
+if($kullanici_adi==$_POST['id'] && $sifre==$_POST['sifre'])
+{
     echo "Hoşgeldiniz b231210577 <br/>";
-    echo "7 saniye içinde hakkimda sayfasına yönlendirileceksiniz";
-    exit(); // Yönlendirme yapıldıktan sonra kodun çalışmasını durdurmak için exit() kullanılmalı
-} else {
-    header("Refresh: 3; url=login.html");
+    echo " 7 saniye içinde hakkimda sayfasına yönelendirileceksiniz";
+    header("Refresh: 7; url=index.html");
+
+}
+else{
     echo "Girilen bilgiler hatalıdır. 3 saniye içinde giriş ekranına yönelendirileceksiniz";
-    exit(); // Yönlendirme yapıldıktan sonra kodun çalışmasını durdurmak için exit() kullanılmalı
+    header("Refresh: 3; url=login.html");
 }
 
 ?>
