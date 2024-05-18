795e6e761b793f982e3585aeb11d9c4e608d5a12
MUAZ SWİS <muazswis@gmail.com>
2024-05-18 17:13:13 +0300

    guncelleme

--- iletiЯim.php
+++ iletiЯim.php
@@ -1,9 +1,23 @@
+
 <?php
- echo "Adı:".$_POST['ad'];
- echo "<br>Soyadı:".$_POST['soyad'];
- echo "<br>Doğum Tarihi:".$_POST['doğum'];
- echo "<br>Eğitim Düzeyi:".$_POST['eğitim'];
- echo "<br>Email:".$_POST['email'];
- echo "<br>Cinsiyet:".$_POST['cns'];
- echo "<br>Kullanıcı Mesajı:".$_POST['mesaj'];
- ?>
+ // Form verilerini işlemek için gerekli kontrolleri yapın
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formun daha önce gönderilip gönderilmediğini kontrol edin
+    if (!isset($_POST['submitted'])) {
+        // Formun ilk kez gönderildiğini belirtmek için submitted adında bir gizli alan ekleyin
+        echo "<input type='hidden' name='submitted' value='true'>";
+
+        // Form verilerini ekrana yazdırın
+        echo "Adı:" . $_POST['ad'];
+        echo "<br>Soyadı:" . $_POST['soyad'];
+        echo "<br>Doğum Tarihi:" . $_POST['doğum'];
+        echo "<br>Eğitim Düzeyi:" . $_POST['eğitim'];
+        echo "<br>Email:" . $_POST['email'];
+        echo "<br>Cinsiyet:" . $_POST['cns'];
+        echo "<br>Kullanıcı Mesajı:" . $_POST['mesaj'];
+    } else {
+        // Formun zaten gönderildiğini belirtir
+        echo "Form zaten gönderildi!";
+    }
+}
+?>
